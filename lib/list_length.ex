33dd675cad75bdaf2c9a0749b940beaec3a2bdbf
HiiMartins <higor.dsm10@gmail.com>
2021-12-07 09:56:53 -0300

    implementation of call and count_list function in ListLength method

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    count_list(list, 0)
+  end
+  defp count_list([_head | tail], acc) do
+    acc = acc + 1
+    count_list(tail, acc)
+  end
+  defp count_list([], acc) do
+    acc
   end
 end
